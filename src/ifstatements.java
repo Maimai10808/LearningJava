5e3fc7a0261e9ef3cdbc422be7dfa20c5c3a545d
Maimai10808 <331686549@qq.com>
2025-08-10 23:40:46 +0800

    7.if statements

---
+++ src/ifstatements.java
@@ -0,0 +1,15 @@
+public class ifstatements {
+    static  void main(String args[]) {
+
+// if statement = performs a block of code if its condition is true
+
+        boolean isStudent = true;
+
+        if(isStudent){
+            System.out.println("You are a student! 🏫");
+        }
+        else{
+            System.out.println("You are NOT a student 🏢");
+        }
+    }
+}
