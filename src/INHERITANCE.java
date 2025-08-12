282b4f9cb0e533c37b897f6b5fb1b518f0115972
Maimai10808 <331686549@qq.com>
2025-08-12 09:55:40 +0800

    44. INHERITANCE

---
+++ src/INHERITANCE.java
@@ -0,0 +1,66 @@
+public class INHERITANCE {
+    public static void main(String[] args) {
+
+        // Inheritance = One class inherits the attributes and methods
+        //                         from another class.
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+    }
+}
+
+
+//public class Organism {
+//
+//    boolean isAlive;
+//
+//    Organism(){
+//        isAlive = true;
+//    }
+//}
+
+
+
+//public class Plant extends Organism{
+//
+//    void photosynthesize(){
+//        System.out.println("The plant absorbs sunlight");
+//    }
+//}
+
+
+//public class Animal extends Organism{
+//
+//    void eat(){
+//        System.out.println("This animal is eating");
+//    }
+//}
+//public class Dog extends Animal{
+//
+//    int lives = 1;
+//
+//    void speak(){
+//        System.out.println("The dog goes *woof*");
+//    }
+//}
+//public class Cat extends Animal{
+//
+//    int lives = 9;
+//
+//    void speak(){
+//        System.out.println("The cat goes *meow*");
+//    }
+//}
+
+/*
+class 子类 extends 父类 {
+    // 子类可以有自己独特的属性和方法
+}
+
+	•	extends 关键字表示继承关系。
+	•	子类会自动拥有父类的非私有成员（private 成员不能直接访问）。
+
+
+ */
