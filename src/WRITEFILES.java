a0bf247dbe78440a63a3c104bd96dc733c264c40
Maimai10808 <331686549@qq.com>
2025-08-17 15:50:08 +0800

    59 60 ：WRITE FILES & READ FILES

--- src/WRITEFILES.java
+++ src/WRITEFILES.java
@@ -1,6 +1,10 @@
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class WRITEFILES {
     public static void main(String[] args) {
@@ -12,7 +16,7 @@ public class WRITEFILES {
         // PrintWriter = Best for structured data, like reports or logs
         // FileOutputStream = Best for binary files (e.g., images, audio files)
 
-        String filePath = "test.txt";
+        String filePath1 = "test.txt";
         String textContent = """
                 Roses are Red
                 Violets are Blue
@@ -20,7 +24,7 @@ public class WRITEFILES {
                 ROCKIN' EVERWHERE!
                 """;
 
-        try(FileWriter writer = new FileWriter(filePath)){
+        try(FileWriter writer = new FileWriter(filePath1)){
             writer.write(textContent);
             System.out.println("File has been written");
         }
@@ -29,6 +33,28 @@ public class WRITEFILES {
         }
         catch(IOException e){
             System.out.println("Could not write file");
+        }
+
+
+        // How to read a file using Java (3 popular options)
+
+        // BufferedReader + FileReader: Best for reading text files line-by-line
+        // FileInputStream: Best for binary files (e.g., images, audio files)
+        // RandomAccessFile: Best for read/write specific portions of a large file
+
+        String filePath = "C:\\Users\\User\\Desktop\\test.txt";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong");
         }
     }
 }
